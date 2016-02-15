26b8c0fbe590ba9cf17319fd2d5fac47395c14be
Al Imran Suvro <bd2010news@gmail.com>
2016-02-15 11:56:31 +0600

    Create infinityLoopThread.java

---
+++ infinityLoopThread.java
@@ -0,0 +1,16 @@
+        thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this,"the toast no:"+ i++,Toast.LENGTH_LONG).show();
+
+                try {
+                    thread.sleep(1000);
+                    handler.postDelayed(this, 5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        });
+        thread.run();
